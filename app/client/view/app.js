db16cb35f4d4c31ca44a6c3a1a0dc5cacd77fb25
MahmoudKM <mahmoudkm@hotmail.com>
2016-02-09 16:50:14 +0200

    Added select box with OSM, MapQuest and Bing maps switcher

--- app/client/view/app.js
+++ app/client/view/app.js
@@ -1,2 +1,44 @@
-// Template.mapDemo.helpers({
-// });
+var generateMap = function(layerSource){
+	
+	var layer = new ol.layer.Tile();
+
+	var map = new ol.Map({
+	  layers:[layer],	  
+	  target: 'map',
+	  view: new ol.View({
+	    center: [0, 0],
+	    zoom: 2
+	  })
+
+	});
+
+	if(layerSource == "osm"){		
+		layer.setSource(new ol.source.OSM());
+
+	}else if(layerSource == "msBing"){
+		layer.setSource(new ol.source.BingMaps({
+		            	 key: '',
+		            	 imagerySet: 'Aerial'
+		          	 	}));
+
+	}else if(layerSource == "mapQuest"){
+		layer.setSource(new ol.source.MapQuest({layer: 'sat'}));
+
+	}
+
+
+}
+
+
+Template.mapDemo.events({
+  'change .mapSourceSelector': function(e) {
+  		generateMap($(e.target).val());
+  }
+});
+
+Template.mapDemo.rendered = function(){
+	generateMap('osm');
+};
+
+
+
